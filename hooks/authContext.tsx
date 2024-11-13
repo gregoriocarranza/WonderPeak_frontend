4fc0027015eb74b771e0480b16e9f8e63fb69b1d
Dani Knoska <danisdnk>
2024-11-13 04:20:28 -0300

    modify: auth context to add userInfo persistant

--- hooks/authContext.tsx
+++ hooks/authContext.tsx
@@ -1,9 +1,11 @@
 import React, { createContext, useContext, useState, ReactNode, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { UserInfo } from "@/types/interfaces";
 
 type AuthContextType = {
   token: string | null;
   isAuthenticated: boolean;
+  userMe: (user: string) => void;
   login: (token: string) => void;
   logout: () => void;
 };
@@ -12,14 +14,23 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [token, setToken] = useState<string | null>(null);
+  const [userInfo, setUserInfo] = useState<string | null>(null);
 
   useEffect(() => {
     const loadToken = async () => {
       const savedToken = await AsyncStorage.getItem("token");
       if (savedToken) setToken(savedToken);
     };
+
+    const loadUserMe = async () => {
+      const savedUser = await AsyncStorage.getItem("user");
+      if (savedUser) setUserInfo(savedUser);
+    };
+
     loadToken();
-  }, []);
+    loadUserMe();
+
+  }, [userInfo, token]);
 
   const login = async (newToken: string) => {
     setToken(newToken);
@@ -29,11 +40,19 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const logout = async () => {
     setToken(null);
     await AsyncStorage.removeItem("token");
+    await AsyncStorage.removeItem("user");
+
+  };
+
+  const userMe = async (user: any) => {
+    setUserInfo(user);
+    await AsyncStorage.setItem("user", JSON.stringify(user));
   };
 
   const value: AuthContextType = {
     token,
     isAuthenticated: !!token,
+    userMe,
     login,
     logout,
   };
---
+++ types/interfaces.ts
@@ -0,0 +1,13 @@
+export type UserInfo = {
+    userUuid: string;
+    name: string;
+    lastname: string;
+    nickname: string;
+    email: string;
+    profileImage: string;
+    coverImage: string;
+    description: string;
+    gender: string;
+    gamificationLevel: string;
+    active: string;
+};
