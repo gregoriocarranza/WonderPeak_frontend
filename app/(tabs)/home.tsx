2403609e2445afc13531a3aa70f057830b173e09
Carolina Belen Maio <cmaio@MacBook-Air-de-Carolina.local>
2024-11-14 18:29:58 -0300

    add: empty state at home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, FlatList, View, ActivityIndicator} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  FlatList,
+  View,
+  ActivityIndicator,
+  Image,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HeaderHome from "@/components/HeaderHome";
 import { Colors } from "@/constants/Colors";
 import { useAuth } from "@/hooks/authContext";
 import PostItem from "@/components/Post";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { images } from "@/constants";
+import i18n from "@/languages";
 
 export default function Home() {
-  const { token } =  useAuth();
+  const { token } = useAuth();
   const [refreshing, setRefreshing] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleRefresh = (): void => {
     setRefreshing(true);
-    fetchUserFeed()
+    fetchUserFeed();
     setTimeout(() => {
       setRefreshing(false);
     }, 2000);
@@ -35,22 +44,24 @@ export default function Home() {
         return;
       }
 
-      const response = await fetch("https://wonderpeak.uade.susoft.com.ar/api/posts/feed?page=0&limit=20", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${savedToken}`,
-        },
-      });
+      const response = await fetch(
+        "https://wonderpeak.uade.susoft.com.ar/api/posts/feed?page=0&limit=20",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${savedToken}`,
+          },
+        }
+      );
 
       if (!response.ok) {
         throw new Error("Error al obtener la información del usuario");
       }
       //!! aca hay que trabajar un poco mas esto para que se pueda recuperar del Context si la info ya esta
       const data = await response.json();
-      console.log(data, "USER FEED")
-      setUserFeed(data)
-
+      console.log(data, "USER FEED");
+      setUserFeed(data);
     } catch (error) {
       console.log(error);
     } finally {
@@ -62,7 +73,6 @@ export default function Home() {
     fetchUserFeed();
   }, []);
 
-
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: Colors.white }}>
       {loading && (
@@ -72,17 +82,26 @@ export default function Home() {
       )}
 
       <HeaderHome />
-      <FlatList
-        data={userFeed?.data}
-        keyExtractor={(item, index) => `${item.postUuid}-${index}`}
-        renderItem={({ item }) => (
-          <View className="mb-4">
-            <PostItem {...item} />
-          </View>
-        )}
-        refreshing={refreshing}
-        onRefresh={handleRefresh}
-      />
+      {userFeed?.data && userFeed.data.length ? (
+        <FlatList
+          data={userFeed?.data}
+          keyExtractor={(item, index) => `${item.postUuid}-${index}`}
+          renderItem={({ item }) => (
+            <View className="mb-4">
+              <PostItem {...item} />
+            </View>
+          )}
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
+        />
+      ) : (
+        <View style={styles.modalContent}>
+          <Image source={images.emptyState} style={styles.image} />
+          <Text className="font-pbold" style={styles.text}>
+            {i18n.t("emptyState")}
+          </Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
@@ -98,5 +117,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#00000033",
     zIndex: 1,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    maxWidth: "100%",
+  },
+  modalContent: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 9,
+  },
+  text: {
+    fontSize: 32,
+    textAlign: "center",
+    width: "80%",
+    color: Colors.light.text,
   },
 });
